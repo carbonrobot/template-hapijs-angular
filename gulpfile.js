32eaed08e1420e125f72ece939e258ee38807508
Charlie Brown <carbonrobot@gmail.com>
2015-08-31 13:05:17 -0500

    Refactor build file

--- gulpfile.js
+++ gulpfile.js
@@ -7,41 +7,73 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     sourcemaps = require('gulp-sourcemaps'),
     templateCache = require('gulp-angular-templatecache'),
-    bower = require('bower'),
+    bower = require('gulp-bower'),
     wrap = require('gulp-wrap'),
     ngannotate = require('gulp-ng-annotate'),
     less = require('gulp-less'),
     jshint = require('gulp-jshint'),
     config = require('./gulpfile.config'),
     jasmine = require('gulp-jasmine'),
+    runSequence = require('run-sequence'),
     KarmaServer = require('karma').Server;
 
 // tasks
-gulp.task('default', ['build']);
-gulp.task('serve', ['build', 'watch'], startDevMode);
-gulp.task('build', ['clean', 'bower'], build);
-gulp.task('watch', watch);
-gulp.task('clean', cleanBuildFiles);
-gulp.task('bower', downloadPackages);
-gulp.task('test:api', testApi);
-gulp.task('test:ui', function(done){
-    testUi(true, done);
+gulp.task('default', ['serve']);
+gulp.task('serve', function(done){
+    runSequence(
+        'bower',
+        'clean',
+        ['vendor', 'styles', 'templates', 'scripts', 'images', 'content'], 
+        'watch',
+        'run',
+        done);
 });
-gulp.task('test:ui:single', function(done){
-    testUi(false, done);
+
+gulp.task('build', function(done){
+    runSequence(
+        'bower',
+        'clean',
+        ['vendor', 'styles', 'templates', 'scripts', 'images', 'content'], 
+        done);
+});
+
+gulp.task('test:api', function(){
+    return gulp.src([
+        'test/sample-api/**/*.spec.js'
+    ])
+    .pipe(jasmine());
+});
+
+gulp.task('test:ui', function(done){
+    new KarmaServer({
+        configFile: __dirname + '/test/sample-ui/karma.conf.js',
+        autoWatch: watch,
+        singleRun: !watch
+    }, function(exitcode){
+        done();
+        process.exit(exitcode);
+    }).start();
 });
 
 // components
-gulp.task('vendor', vendor);
-gulp.task('templates', templates);
-gulp.task('scripts', scripts);
-gulp.task('images', images);
+gulp.task('bower', bower);
+gulp.task('clean', clean);
 gulp.task('content', content);
-gulp.task('styles', ['fonts'], styles);
 gulp.task('fonts', fonts);
+gulp.task('images', images);
+gulp.task('run', run);
+gulp.task('scripts', scripts);
+gulp.task('styles', ['fonts'], styles);
+gulp.task('templates', templates);
+gulp.task('vendor', vendor);
+gulp.task('watch', watch);
 
-function build(){
-    gulp.start('vendor', 'styles', 'templates', 'scripts', 'images', 'content');
+function bower(done){
+    return bower();
+}
+
+function clean(done){
+    del(['public/*'], done);
 }
 
 function content() {
@@ -49,15 +81,9 @@ function content() {
         .pipe(gulp.dest(config.build.output.content));
 }
 
-function cleanBuildFiles(done){
-    del(['public/*'], done);
-}
-
-function downloadPackages(done){
-    bower.commands.install([], {save: true}, {})
-    .on('end', function(installed){
-      done();
-    });
+function fonts() {
+    return gulp.src(config.assets.fonts)
+        .pipe(gulp.dest(config.build.output.fonts));
 }
 
 function images() {
@@ -65,9 +91,13 @@ function images() {
     .pipe(gulp.dest(config.build.output.img));
 }
 
-function fonts() {
-    return gulp.src(config.assets.fonts)
-		.pipe(gulp.dest(config.build.output.fonts));
+function run(){
+    nodemon({ 
+        script: 'index.js',
+        ext: 'js',
+        ignore: ['src/modules/sample-ui/*', 'public/*'],
+        env: {NODE_ENV: 'development', DEBUG: true}
+    });
 }
 
 function scripts() {
@@ -83,15 +113,6 @@ function scripts() {
         .pipe(gulp.dest(config.build.output.js));
 }
 
-function startDevMode(){
-    nodemon({ 
-        script: 'index.js',
-        ext: 'js',
-        ignore: ['src/modules/sample-ui/*', 'public/*'],
-        env: {NODE_ENV: 'development', DEBUG: true}
-    });
-}
-
 function styles() {
     return gulp.src(config.assets.css)
         .pipe(sourcemaps.init())
@@ -105,24 +126,6 @@ function templates() {
     return gulp.src(config.assets.views)
         .pipe(templateCache({ module: config.templateModuleName }))
         .pipe(gulp.dest(config.build.output.js));
-}
-
-function testUi(watch, done){
-    new KarmaServer({
-        configFile: __dirname + '/test/sample-ui/karma.conf.js',
-        autoWatch: watch,
-        singleRun: !watch
-    }, function(exitcode){
-        done();
-        process.exit(exitcode);
-    }).start();
-}
-
-function testApi(done){
-    return gulp.src([
-            'test/sample-api/**/*.spec.js'
-        ])
-        .pipe(jasmine());
 }
 
 function vendor() {
